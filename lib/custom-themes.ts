c9ce7bfbd324073c561da8a112f7a3191ec95e8b
클라우드 <cloud@keullaudeuui-MacBookAir.local>
2026-02-27 16:04:51 +0900

    1

--- lib/custom-themes.ts
+++ lib/custom-themes.ts
@@ -19,14 +19,79 @@ export type CustomThemePreset = {
  */
 export const CUSTOM_THEMES: CustomThemePreset[] = [
   {
+    id: "bg47",
+    label: "BG 47",
+    imageUrl: "/custom-themes/47.jpg",
+  },
+  {
+    id: "bg5159130",
+    label: "BG 5159130",
+    imageUrl: "/custom-themes/5159130.jpg",
+  },
+  {
+    id: "bg5160549",
+    label: "BG 5160549",
+    imageUrl: "/custom-themes/5160549.jpg",
+  },
+  {
+    id: "bg5170396",
+    label: "BG 5170396",
+    imageUrl: "/custom-themes/5170396.jpg",
+  },
+  {
+    id: "bg6487685",
+    label: "BG 6487685",
+    imageUrl: "/custom-themes/6487685.jpg",
+  },
+  {
+    id: "bgSunsetWatercolor",
+    label: "Sunset Watercolor",
+    imageUrl: "/custom-themes/6_sunset_cloudy_sky_watercolor_b.jpg",
+  },
+  {
+    id: "bg7175949",
+    label: "BG 7175949",
+    imageUrl: "/custom-themes/7175949.jpg",
+  },
+  {
+    id: "bg8187310",
+    label: "BG 8187310",
+    imageUrl: "/custom-themes/8187310.jpg",
+  },
+  {
+    id: "bg9051050",
+    label: "BG 9051050",
+    imageUrl: "/custom-themes/9051050.jpg",
+  },
+  {
     id: "akireSurface1280",
     label: "Akire Surface",
     imageUrl: "/custom-themes/akirevarga-surface-4373559_1280.jpg",
   },
   {
+    id: "cozyCatCandle",
+    label: "Cozy Cat Candle",
+    imageUrl: "/custom-themes/cozy-cat-by-candlelight.jpg",
+  },
+  {
     id: "felixNewYear1280",
     label: "Felix New Year",
     imageUrl: "/custom-themes/felix-mittermeier-new-year-background-3625405_1280.jpg",
+  },
+  {
+    id: "ksushlaBackground",
+    label: "Ksushla Background",
+    imageUrl: "/custom-themes/ksushlapush-background-6556413.jpg",
+  },
+  {
+    id: "rm456004",
+    label: "RM456 004",
+    imageUrl: "/custom-themes/rm456-004.jpg",
+  },
+  {
+    id: "v960Ning31",
+    label: "V960 Ning 31",
+    imageUrl: "/custom-themes/v960-ning-31.jpg",
   },
   {
     id: "walWater1280",
